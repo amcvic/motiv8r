3ca05857f6ccfabc6fc144b44b4350ccbe8d565b
foxbeckya <foxbeckya@gmail.com>
2019-08-31 09:19:47 -0400

    add buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+
 
 @NgModule({
   declarations: [
@@ -12,8 +16,11 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
