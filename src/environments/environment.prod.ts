3e559d6f8c72211917ea9ab5f6343471df6b3405
austin <austinmcvicker@twh-mbp.local>
2019-09-14 11:02:26 -0400

    fix environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export let APIURL = '';
 
 switch (window.location.hostname) {
   case 'am-motiv8r.herokuapp.com':
-    APIURL = 'https://am-motiv8r.herokuapp.com'
+    APIURL = 'https://am-motiv8r-server.herokuapp.com'
     break;
   default:
     APIURL = 'http://localhost:3000';
